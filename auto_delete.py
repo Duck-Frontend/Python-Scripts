c36e70d1c193d573539234bff3a3f3107c3e4b1b
Anton Stadolnik <stadolnikanton1@gmail.com>
2025-08-14 21:46:47 +0300

    Скрипт для удаления файлов в указанном каталоге

---
+++ auto_delete.py
@@ -0,0 +1,51 @@
+"""
+Скрипт для автоматического удаления всех файлов в нужной директории
+"""
+
+
+import os
+
+
+def get_path_for_clean():
+    dirs = []
+
+    print(f"Текущая директория: {os.getcwd()}")
+
+    for item in os.listdir():
+        if os.path.isdir(item):
+            dirs.append(item)
+    print("Директории в текущем каталоге:", end=" ")
+
+    for catalog in dirs:
+        print(catalog, end=" ")
+
+    path = input("\nВведите имя директории для отчистки: ")
+    abs_path = os.path.abspath(path)
+
+    if os.path.exists(path):
+        print("Начинаю отчистку")
+        remove_all_files(abs_path)
+    else:
+        print("Нет такого пути")
+
+
+def remove_all_files(path):
+    os.chdir(path)
+    files = os.listdir()
+
+    for file in files:
+        print(file, sep=",", end=" ")
+
+    answer = int(input("\nУдалить все эти файлы?\n1: Да 2: Нет\n: "))
+
+    if answer == 1:
+        for file in files:
+            os.remove(file)
+        else:
+            print("Все Файлы удалены")
+
+    else:
+        print("Всего хорошего!")
+
+
+get_path_for_clean()
